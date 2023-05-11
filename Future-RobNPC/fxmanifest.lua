700dea726f5530e4349560575087f35750e3eba7
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-11 11:25:27 -0700

    Create fxmanifest.lua

---
+++ Future-RobNPC/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Awesome Gaming'
+description 'A script that allows players to rob NPCs at gunpoint simply by aiming a gun at them.'
+version '1.0.0'
+
+shared_scripts {
+    'Config.lua'
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
